fc0aa2e39f6ebb13f8a833f0c5e4a661e9c186db
Abdiasiis halane <112101372+abdiasiis78@users.noreply.github.com>
2022-12-30 10:12:57 +0300

    waan ka shaqeeyey manta asignmentigi

--- components/Menu.js
+++ components/Menu.js
@@ -25,10 +25,52 @@ let menuItems = [
   
     Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
   
-    Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+    Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu
+     (your div with a 'menu' class).
   
     Step 5: Don't forget to return your div.menu.
   
     Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
   */
-  z
+  
+  function menuMaker(menuItems) {
+    // Step 1: Create the menu element
+    const manuElement = document.createElement('div')
+    manuElement.classList.add('menu')
+
+    // Step 2: Create the list element and add the menu items
+     const listElement = document.createElement('ul')
+     menuItems.forEach(items => {
+      const listItemElement = document.createElement('li')
+      listItemElement.textContent = items
+      listElement.appendChild(listItemElement)
+
+     });
+
+     manuElement.appendChild(listElement)
+
+     // Step 3: Select the menu button
+
+     const menuButton = document.querySelector('.menu-button')
+
+      // Step 4: Add a click event listener to the menu button
+
+      menuButton.addEventListener('click', function(){
+       manuElement.classList.toggle('menu--open')
+
+      })
+
+       // Step 5: Return the menu element
+          return manuElement;
+  
+  }
+
+  // Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array
+
+  const menu = menuMaker(menuItems)
+
+  // Append the returned menu to the header
+
+  const header = document.querySelector('.header')
+
+  header.appendChild(menu)
